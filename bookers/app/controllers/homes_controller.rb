a09386295ad233eb201af7409db0ef005c45c619
hamumelo <ec2-user@ip-172-31-27-124.ec2.internal>
2021-07-24 10:17:29 +0000

    save2

--- bookers/app/controllers/homes_controller.rb
+++ bookers/app/controllers/homes_controller.rb
@@ -1,13 +1,5 @@
 class HomesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def edit
+  def top
+    # @homes = homes.all
   end
 end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :s
+  
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # get 'top' => 'homes#top'
+  root to: 'books#top'
+  get 'top' => 'books#top'
+  resources :books
+  # resources :homes
 end
